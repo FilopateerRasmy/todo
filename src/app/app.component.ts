9dc72a1596a67ba5411edef938492b6c449c9e9d
Filopateer <filopateer.rasmy@outlook.com>
2023-06-12 12:03:33 +0300

    todo task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+  form = new FormGroup({
+    todo: new FormControl('', []),
+  });
+
+  list: string[] = [];
+  checkedItems:{[key:string]:boolean} = {};
+  getFormField(field: string) {
+    return this.form.get(field) as FormControl;
+  }
+
+  save() {
+    this.list.push(this.getFormField('todo').value);
+    this.form.reset();
+  }
+
+  public onCheckboxChange(item: string) {
+    this.checkedItems[item] = !this.checkedItems[item];
+  }
 }
